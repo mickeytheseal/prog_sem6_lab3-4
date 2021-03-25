b2dc5d2ec353db6f15c44eae6a6c642f3066821e
Mickey <42099345+mickeytheseal@users.noreply.github.com>
2021-03-25 21:49:32 +0300

    Added stud_id generator

--- src/com/company/Student.java
+++ src/com/company/Student.java
@@ -1,7 +1,20 @@
 package com.company;
 
+
+import java.util.Calendar;
+
 public class Student extends Enrollee{
-    public Student(String name, int age, int score, String id) {
-        super(name, age, score, id);
+    private static int code = 100;
+    private final String stud_id;
+    private int[][] marks;
+
+    public Student(Enrollee enrollee){
+        super(enrollee.name, enrollee.age, enrollee.score, enrollee.id);
+        super.major = enrollee.major;
+        int year = Calendar.getInstance().get(Calendar.YEAR);
+        stud_id = String.valueOf(year).substring(2) + major.name().substring(0,2) + String.valueOf(code);
+        code++;
     }
+
+    public String getStud_id(){ return stud_id; }
 }
