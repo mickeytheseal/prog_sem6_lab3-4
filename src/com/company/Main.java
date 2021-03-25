9b3180986a7ac3d66132f8d22899fd0fd95dcd01
Mickey <42099345+mickeytheseal@users.noreply.github.com>
2021-03-25 21:48:38 +0300

    Minor changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,8 +7,12 @@ public class Main {
     public static void main(String[] args) {
         Enrollee e1 = new Enrollee("Alex",18,250,"4514 563221");
         Major[] budget_majors = e1.getBudgetMajors();
-        System.out.println(Arrays.toString(budget_majors));
-        e1.setMajor(budget_majors[1]);
-        System.out.println(e1.getMajor().name());
+        Major[] majors = Major.values();
+        System.out.println("Majors: " + Arrays.toString(majors));
+        System.out.println("Available budget majors: " + Arrays.toString(budget_majors));
+        e1.setMajor(majors[1]);
+        System.out.println("You picked " + e1.getMajor().name());
+        Student s1 = new Student(e1);
+        System.out.println(s1.getStud_id());
     }
 }
