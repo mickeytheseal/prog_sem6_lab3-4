2de17d0ff0e78db7c65beec3a8ef94c9ee5c02cf
Mickey <42099345+mickeytheseal@users.noreply.github.com>
2021-03-25 21:48:26 +0300

    Added main functional

--- src/com/company/Extrabudgetary.java
+++ src/com/company/Extrabudgetary.java
@@ -1,7 +1,38 @@
 package com.company;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class Extrabudgetary extends Student{
-    public Extrabudgetary(String name, int age, int score, String id) {
-        super(name, age, score, id);
+    private String contract;
+    private Date expiration_date;
+    private boolean is_paid;    //оплачен ли следующий семестр
+
+    public Extrabudgetary(Student student) {
+        super(student);
+        Date date = new Date();
+        Calendar c = Calendar.getInstance();
+        c.setTime(date);
+        c.add(Calendar.MONTH, 4);
+        expiration_date = c.getTime();
+        is_paid = true;
+    }
+
+    public String getContract(){ return contract; }
+    public boolean getIsPaid(){ return is_paid; }
+    public void setPaid(){ is_paid = true; }
+
+
+    public void extendContract(){
+        if (is_paid){
+            Calendar c = Calendar.getInstance();
+            c.setTime(expiration_date);
+            c.add(Calendar.MONTH, 4);
+            expiration_date = c.getTime();
+            is_paid = false;
+        }
+        else {
+            System.out.println("Student hasn't paid yet. Notification sent.");
+        }
     }
 }
