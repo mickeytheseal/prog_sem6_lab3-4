5ee2598208a4c4a3baccd326ef4c810d1f8b8e34
Mickey <42099345+mickeytheseal@users.noreply.github.com>
2021-05-09 23:40:28 +0300

    Added setter and getter for marks

--- src/com/company/Student.java
+++ src/com/company/Student.java
@@ -1,13 +1,13 @@
 package com.company;
 
-
-import java.util.Arrays;
-import java.util.Calendar;
+import java.util.*;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public abstract class Student extends Enrollee{
     private static int code = 100;
     protected final String stud_id;
-    private int[][] marks; //для лабы 4
+    private ArrayList<Mark> marks;
 
     public Student(Enrollee enrollee){
         super(enrollee.name, enrollee.age, enrollee.score, enrollee.id);
@@ -15,26 +15,26 @@ public abstract class Student extends Enrollee{
         int year = Calendar.getInstance().get(Calendar.YEAR);
         stud_id = String.valueOf(year).substring(2) + major.name().substring(0,2) + String.valueOf(code);
         code++;
-        marks = new int[8][6]; //[семестр][предмет]
+        marks = new ArrayList<>();
     }
 
     public Student(Student student){
         super(student.name, student.age, student.score, student.id);
         super.major = student.major;
         stud_id = student.stud_id;
-
-        marks = new int[8][6]; //[семестр][предмет]
     }
 
     public String getStud_id(){ return stud_id; }
 
-    // TODO: 26.03.2021 придумать нормальный нейминг и поставить ограничение на оцеки
-    public void setMarks(int sem, int predm, int value){
-        marks[sem][predm] = value;
+
+    public void setMark(int value, int term, Subject subject){
+        Mark new_mark = new Mark(value,term,subject);
+        Stream<Mark> stream = marks.stream();
+        stream.filter(mark -> mark.equals(new_mark)).findAny().ifPresent(to_change -> marks.remove(to_change));
+        marks.add(new_mark);
     }
 
-    public String getMarks(){
-        return Arrays.deepToString(marks);
+    public ArrayList<Mark> getMarks(){
+        return marks;
     }
-
 }
