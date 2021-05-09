d9fbd3c7f77daba51bdcf08f8b6689cdc42276b6
Mickey <42099345+mickeytheseal@users.noreply.github.com>
2021-05-09 23:41:03 +0300

    Added Subject enum
    To store subjects

---
+++ src/com/company/Subject.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public enum Subject {
+    MATH,
+    LANGUAGE,
+    HISTORY,
+    LAW,
+    JAVA,
+    MANAGEMENT,
+    STATISTICS,
+    FINANCES,
+    DATABASES,
+    JAVA_SPRING,
+    WEB;
+}
